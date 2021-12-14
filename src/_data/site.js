87df9cf5eb1444515def275c323a44f96a38d4a0
gSARC <95992097+gSARC@users.noreply.github.com>
2021-12-13 23:34:36 -0500

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 // Some useful variables to expose to our site templsates
-require('dotenv').config();
+require('env').config();
 
 module.exports = {
   "utm": "?utm_source=github&utm_medium=hellotrello-pnh&utm_campaign=devex",
