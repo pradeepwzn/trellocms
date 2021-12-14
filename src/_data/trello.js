23d9ff530e1a21179c0cfca1de8dd90a449a4c3b
gSARC <95992097+gSARC@users.noreply.github.com>
2021-12-13 22:31:50 -0500

    Update trello.js

--- src/_data/trello.js
+++ src/_data/trello.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 // With public fallbacks for happier onboarding
 require('dotenv').config();
 const {
-  TRELLO_BOARD_URL='https://trello.com/b/rVn5ROBR/gsarccms',
+  TRELLO_BOARD_URL='https://trello.com/b/n7ggwrak/gsarctest',
   TRELLO_LIST_ID='5e98325d6d6bd120f2b7395f',
   BRANCH } = process.env;
 
